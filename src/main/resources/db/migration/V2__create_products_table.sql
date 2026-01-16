0ee232a1bf0f956ddeccb81953261182c036dde6
Yrysas <yrysssdaulet@gmail.com>
2026-01-16 19:35:23 +0500

    step1 30% proj

--- src/main/resources/db/migration/V2__create_products_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE products (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price NUMERIC(10,2) NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
-);
-
-INSERT INTO products (name, description, price) VALUES ('Sample product', 'A demo product', 19.99);
